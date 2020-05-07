e6d38fe3ebb200c1a8cf3af97ad36b82a3ed023b
🌵 Régis Cassier <regis.cassier@orange.fr>
2020-05-07 17:33:40 +0200

    add select player + link to next scene

--- TestOculus/Assets/scripts/UI_Menu_chooseRole.cs
+++ TestOculus/Assets/scripts/UI_Menu_chooseRole.cs
@@ -2,51 +2,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UI_Menu_chooseRole : MonoBehaviour
 {
-
-    private string currentPlayer;
-    private int nbDieu = 1;
-    private int nbPecheur = 1;
-
-    public Text DieuLabel;
-    public Text PecheurLabel;
     public Button ValidBtn;
+    public Text PecheurLabel;
+    public Text DieuLabel;
 
-    private HashSet<String> dieu;
-    private HashSet<String> pecheur;
+    private bool isDieu;
+    private bool isPecheur;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.currentPlayer = "a";
+        this.isDieu = false;
+        this.isPecheur = false;
+
         this.ValidBtn.interactable = false;
-
-        this.dieu = new HashSet<String>();
-        this.pecheur = new HashSet<String>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void setPlayerAsDieu()
     {
-        this.dieu.Add(this.currentPlayer);
-        this.pecheur.Remove(this.currentPlayer);
+        this.isDieu = true;
+        this.isPecheur = false;
         updateLabels();
         Debug.Log("Click to setPlayerAsDieu");
     }
 
     public void setPlayerAsPecheur()
     {
-        this.pecheur.Add(this.currentPlayer);
-        this.dieu.Remove(this.currentPlayer);
+        this.isDieu = false;
+        this.isPecheur = true;
         updateLabels();
         Debug.Log("Click to setPlayerAsPecheur");
     }
@@ -54,26 +48,24 @@ public class UI_Menu_chooseRole : MonoBehaviour
     public void validatePlayerChoice()
     {
         Debug.Log("Click to validatePlayerChoice");
+        SceneManager.LoadScene("Scenes/SampleScene", LoadSceneMode.Single);
     }
 
     private void updateLabels()
     {
-        if (this.dieu.Contains(this.currentPlayer)) {
+        if (this.isDieu) {
             this.DieuLabel.color = new Color(255, 0, 0);
         } else {
             this.DieuLabel.color = new Color(0, 0, 0);
         }
 
-        if(this.pecheur.Contains(this.currentPlayer)) {
+        if(this.isPecheur) {
             this.PecheurLabel.color = new Color(255, 0, 0);
         } else {
             this.PecheurLabel.color = new Color(0, 0, 0);
         }
 
-        this.PecheurLabel.text = "Pêcheur\n" + this.pecheur.Count + "/" + this.nbPecheur;
-        this.DieuLabel.text = "Dieu\n" + this.dieu.Count + "/" + this.nbDieu;
-        if (this.pecheur.Contains(this.currentPlayer) || this.dieu.Contains(this.currentPlayer))
-        {
+        if (this.isPecheur || this.isDieu) {
             this.ValidBtn.interactable = true;
         }
     }
