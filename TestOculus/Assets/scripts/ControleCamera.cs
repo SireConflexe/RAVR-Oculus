b9d6431356a715877c6bf0e134adcc14678b2538
Zer4k91 <39822800+Zer4k91@users.noreply.github.com>
2020-05-11 14:51:34 +0200

    Teleportation du joueur (piege)

---
+++ TestOculus/Assets/scripts/ControleCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleCamera : MonoBehaviour
+{
+    public GameObject joueur;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - joueur.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = joueur.transform.position + offset;
+    }
+}
