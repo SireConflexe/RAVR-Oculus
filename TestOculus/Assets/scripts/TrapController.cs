33ead61c8750377172cf0f5ad7de9441f75a4d67
Thibaud Germain <t.germain2@gmail.com>
2020-05-11 17:22:30 +0200

    piege trappe balles

---
+++ TestOculus/Assets/scripts/TrapController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Controllables;
+
+public class TrapController : MonoBehaviour
+{
+    public VRTK.Controllables.ArtificialBased.VRTK_ArtificialRotator rotator;
+    public GameObject trap = null;
+
+
+    protected virtual void OnEnable()
+    {
+        rotator = (rotator == null ? GetComponent<VRTK.Controllables.ArtificialBased.VRTK_ArtificialRotator>() : rotator);
+        rotator.MaxLimitReached += MaxLimitReached;
+    }
+
+    protected virtual void MaxLimitReached(object sender, ControllableEventArgs e)
+    {
+        this.trap.transform.Rotate(new Vector3(-160.0f, 0.0f, 0.0f));
+        this.rotator.isLocked = true;
+    }
+}
